1c21fd8d573be881169c1fdeb81adac5780c1d70
JoowonJang <wndnjs5248@hanmail.net>
2024-07-05 20:58:24 +0900

    init: pages 폴더 추가
    - pages 폴더 추가
    - 예시용 home.js와 index.html파일 및 vite config코드 추가

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src', 'index.html'),
+        home: resolve(__dirname, 'src', '/pages/home/index.html'),
       },
     },
   },
