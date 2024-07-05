6050dd82e2092c2c95a7c3a53366edc2e83278c2
JoowonJang <wndnjs5248@hanmail.net>
2024-07-05 20:46:04 +0900

    init: root 변경 및 build 설정
    - 프로젝트 directory의 root를 src 폴더로 설정
    - npx run build 스크립트 명령을  실행할 때, src 폴더 안의 index.html 파일을 찾도록 설정
    - npx run build 스크립트 명령을 실행할 때, docs 폴더를 지우고 build 명령을 실행하도록 설정
    - '@' 별칭으로 src 폴더를 찾을 수 있도록 설정

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
+  publicDir: resolve(__dirname, 'public'),
   build: {
-    outDir:'docs',
+    outDir: resolve(__dirname, 'docs'),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        
+        main: resolve(__dirname, 'src', 'index.html'),
       },
     },
-    
   },
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
