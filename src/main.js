33b51897798a3b012bc17426deea1a772325cd59
JoowonJang <wndnjs5248@hanmail.net>
2024-07-06 20:47:27 +0900

    feat: 홈 페이지 UI 1차 완성
    - 추천 글 슬라이드는 swiper를 사용함
    - 컴포넌트 분리 필요
    - 반응형UI 검토 필요

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 import '/styles/global.css';
+
+import Swiper from 'swiper';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination, Autoplay],
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return `
+        <span aria-label="현재 슬라이드 번호" class="${currentClass}"></span>
+        <svg aria-hidden="true" width="41" height="2" viewBox="0 0 41 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_238_3875)">
+          <rect y="0.5" width="41" height="1" fill="white"/>
+          <rect y="0.5" width="9" height="1" fill="white"/>
+          </g>
+          <defs>
+          <clipPath id="clip0_238_3875">
+          <rect width="41" height="1" fill="white" transform="translate(0 0.5)"/>
+          </clipPath>
+          </defs>
+        </svg>
+        <span aria-label="전체 슬라이드 수" class="${totalClass}"></span>
+      `;
+    },
+  },
+});
