9ce8a56060887fd22f9ef4986463328cca7408b1
JoowonJang <wndnjs5248@hanmail.net>
2024-07-08 02:24:48 +0900

    feat: 거래글 상세페이지 UI 1차 완성
    - 판매물품 이미지는 swiper를 사용함
    - 컴포넌트 분리 필요
    - 반응형 UI 검토 필요

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src', 'index.html'),
-        home: resolve(__dirname, 'src', '/pages/home/index.html'),
+        exchangeBoard: resolve(
+          __dirname,
+          'src',
+          'pages',
+          'exchangeBoard',
+          'index.html'
+        ),
       },
     },
   },
