2925e46a3530dea9c7241b220ebcf248c1833401
JoowonJang <wndnjs5248@hanmail.net>
2024-07-08 16:12:47 +0900

    refactor: Navigation 컴포넌트 리팩토링
    - 하나의 템플릿으로 인스턴스를 생성한 후에 src 속성을 변경하도록 수정

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -11,51 +11,79 @@ import fullChat from '/assets/icon/full-chat.svg';
 import my from '/assets/icon/my.svg';
 import fullMy from '/assets/icon/full-my.svg';
 
+const navigationTemplate = document.createElement('template');
+navigationTemplate.innerHTML = `
+  <style> ${tailwindCSS} </style>
+  <nav class="fixed bottom-0 w-full max-w-screen-md bg-background px-3 pb-6 pt-2">
+    <ul class="flex justify-between">
+      <li>
+        <a href="/" class="flex flex-col items-center text-sm">
+          <img id="nav-img-home" src=${home} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
+          <span class="text-sm leading-[160%] xs:text-base">홈</span>
+        </a>
+      </li>
+      <li>
+        <a href="/pages/board/" class="flex flex-col items-center text-sm">
+          <img id="nav-img-board" src=${board} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
+          <span class="text-sm leading-[160%] xs:text-base">게시판</span>
+        </a>
+      </li>
+      <li>
+        <a href="/pages/exchange/" class="flex flex-col items-center text-sm">
+          <img id="nav-img-map" src=${map} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
+          <span class="text-sm leading-[160%] xs:text-base">내 근처</span>
+        </a>
+      </li>
+      <li>
+        <a href="pages/chat/" class="flex flex-col items-center text-sm">
+          <img id="nav-img-chat" src=${chat} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
+          <span class="text-sm leading-[160%] xs:text-base">채팅</span>
+        </a>
+      </li>
+      <li>
+        <a href="/pages/profile/" class="flex flex-col items-center text-sm">
+          <img id="nav-img-my" src=${my} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
+          <span class="text-sm leading-[160%] xs:text-base">나의 이듬</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+`;
+
 export class Navigation extends HTMLElement {
   constructor() {
     super();
 
-    const navigationTemplate = document.createElement('template');
-    navigationTemplate.innerHTML = `
-      <style> ${tailwindCSS} </style>
-      <nav class="fixed bottom-0 w-full max-w-screen-md bg-background px-3 pb-6 pt-2">
-        <ul class="flex justify-between">
-          <li>
-            <a href="/" class="flex flex-col items-center text-sm">
-              <img src=${this.dataset.page === '홈' ? fullHome : home} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
-              <span class="text-sm leading-[160%] xs:text-base">홈</span>
-            </a>
-          </li>
-          <li>
-            <a href="/pages/board/" class="flex flex-col items-center text-sm">
-              <img src=${this.dataset.page === '게시판' ? fullBoard : board} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
-              <span class="text-sm leading-[160%] xs:text-base">게시판</span>
-            </a>
-          </li>
-          <li>
-            <a href="/pages/exchange/" class="flex flex-col items-center text-sm">
-              <img src=${this.dataset.page === '내 근처' ? fullMap : map} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
-              <span class="text-sm leading-[160%] xs:text-base">내 근처</span>
-            </a>
-          </li>
-          <li>
-            <a href="pages/chat/" class="flex flex-col items-center text-sm">
-              <img src=${this.dataset.page === '채팅' ? fullChat : chat} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
-              <span class="text-sm leading-[160%] xs:text-base">채팅</span>
-            </a>
-          </li>
-          <li>
-            <a href="/pages/profile/" class="flex flex-col items-center text-sm">
-              <img src=${this.dataset.page === '나의 이듬' ? fullMy : my} alt="홈" aria-hidden="true" class="w-5 xs:w-7" />
-              <span class="text-sm leading-[160%] xs:text-base">나의 이듬</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    `;
-
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(navigationTemplate.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.setNavigationImage();
+  }
+
+  setNavigationImage() {
+    const dataPage = this.getAttribute('data-page');
+    switch (dataPage) {
+      case '홈':
+        this.shadowRoot.getElementById('nav-img-home').src = fullHome;
+        break;
+      case '게시판':
+        this.shadowRoot.getElementById('nav-img-board').src = fullBoard;
+        break;
+      case '내 근처':
+        this.shadowRoot.getElementById('nav-img-map').src = fullMap;
+        break;
+      case '채팅':
+        this.shadowRoot.getElementById('nav-img-chat').src = fullChat;
+        break;
+      case '나의 이듬':
+        this.shadowRoot.getElementById('nav-img-my').src = fullMy;
+        break;
+
+      default:
+        break;
+    }
   }
 }
 
