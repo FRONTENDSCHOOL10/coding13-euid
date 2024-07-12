91146eda26799cb8c600b7a2aa48608c5509b4bb
JoowonJang <wndnjs5248@hanmail.net>
2024-07-13 03:44:58 +0900

    refactor: profile-detail 페이지 성별 선택 버튼 리팩토링 및 체크박스 이벤트 추가
    - 성별 선택 button을 radio input으로 변경
    - 체크박스 크기가 의도한 것보다 작아지는 현상 수정
    - 전체 동의 체크박스르 체크하면 아래의 체크박스가 모두 체크되는 기능 추가
    - 체크박스가 모두 체크되면 아래의 저장 버튼이 활성화되는 기능 추가

--- src/pages/profile-detail/profile-detail.js
+++ src/pages/profile-detail/profile-detail.js
@@ -1,5 +1,10 @@
 const address = document.getElementById('address');
 const searchBtn = document.getElementById('search-btn');
+const agreeMatters = document.getElementById('agree-matters');
+const agreeCheckboxes = agreeMatters.querySelectorAll('input[type="checkbox"]');
+const saveBtn = document.getElementById('save-btn');
+
+console.log(agreeCheckboxes);
 
 function handleSearch() {
   new daum.Postcode({
@@ -11,4 +16,32 @@ function handleSearch() {
   }).open();
 }
 
+function handleAgreeCheck(e) {
+  const target = e.target;
+  if (target.id === 'agree-all') {
+    if (target.checked) {
+      agreeCheckboxes.forEach((box) => {
+        box.checked = true;
+      });
+    } else {
+      agreeCheckboxes.forEach((box) => {
+        box.checked = false;
+      });
+    }
+  }
+
+  for (let i = 1; i < agreeCheckboxes.length; i++) {
+    if (!agreeCheckboxes[i].checked) {
+      saveBtn.classList.remove('bg-secondary', 'cursor-pointer');
+      saveBtn.classList.add('bg-[#D8D9E1]', 'cursor-not-allowed');
+      saveBtn.setAttribute('disabled', true);
+      return;
+    }
+  }
+  saveBtn.removeAttribute('disabled');
+  saveBtn.classList.remove('bg-[#D8D9E1]', 'cursor-not-allowed');
+  saveBtn.classList.add('bg-secondary', 'cursor-pointer');
+}
+
 searchBtn.addEventListener('click', handleSearch);
+agreeMatters.addEventListener('click', handleAgreeCheck);
