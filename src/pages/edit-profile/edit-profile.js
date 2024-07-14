d138c62471bb4abfe4eed55f70bc272043d9020e
aekyung11 <isaiah6014915@gmail.com>
2024-07-15 08:10:53 +0900

    feat: implement profile, edit-profile, profile-detail functionality using web components

---
+++ src/pages/edit-profile/edit-profile.js
@@ -0,0 +1,211 @@
+import { UserService } from '@/service/UserService';
+import tailwindCSS from '/styles/tailwind.css?inline'; // css 파일 inline 가져오기
+import { getPbImageURL } from '@/api/getPbImageURL';
+
+const EditProfileTemplate = document.createElement('template');
+EditProfileTemplate.innerHTML = `
+  <style>${tailwindCSS}</style>
+  <main
+    class="space-y-[0.8125rem] px-[0.75rem] pb-[3rem] pt-[2.8125rem] xs:space-y-[1.1375rem] xs:px-[1.05rem] xs:pb-[4.2rem] xs:pt-[3.9375rem] sm:space-y-[1.4625rem] sm:px-[1.35rem] sm:pb-[5.4rem] sm:pt-[5.0625rem]"
+  >
+    <!-- profile card -->
+    <section
+      class="flex items-center justify-between rounded-[0.5rem] border-[0.01875rem] border-gray-300 p-3 shadow-md xs:rounded-[0.7rem] xs:p-[1.05rem] sm:rounded-[0.9rem] sm:p-[1.35rem]"
+    >
+      <div class="flex flex-col space-y-1 xs:space-y-[0.35rem] sm:space-y-[0.45rem]">
+        <h3 class="username text-base-group font-semibold leading-[1.5]">
+          <span class="sr-only">사용자 닉네임</span>
+        </h3>
+        <span
+          class="text-sm-group h-[1.0625rem] w-fit rounded-full border-[0.04375rem] border-secondary px-1 text-center leading-[1.6] text-secondary xs:h-[1.4875rem] xs:px-[0.35rem] xs:pt-[0.125rem] sm:h-[1.9125rem] sm:w-[2.7rem] sm:pt-[0.0625rem]"
+          ><span class="sr-only">내 기수</span>4기</span
+        >
+      </div>
+      <img
+        src="/assets/avatar-placeholder.png"
+        alt="프로필 이미지"
+        class="avatar-image aspect-square w-[15.625vw] rounded-full [box-shadow:0.25rem_0.25rem_0.25rem_0px_rgba(0,_0,_0,_0.10)]"
+      />
+    </section>
+    <!-- profile card -->
+
+    <!-- information -->
+    <section
+      class="rounded-t-[0.5rem] border-[0.01875rem] border-gray-300 shadow-md xs:rounded-t-[0.7rem] sm:rounded-t-[0.9rem]"
+    >
+      <div
+        class="flex flex-row items-center justify-between border-b-[0.01875rem] border-contentSecondary p-3 pr-4 xs:p-[1.05rem] xs:pr-[1.4rem] sm:p-[1.35rem] sm:pr-[1.8rem]"
+      >
+        <h3 class="text-base-group font-semibold leading-[1.5]">기본 정보</h3>
+        <a href="/pages/profile-detail/" class="text-sm-group text-secondary">수정하기</a>
+      </div>
+      <ul
+        aria-label="기본 정보 리스트"
+        class="space-y-[1.0625rem] px-[0.8125rem] py-[0.625rem] xs:space-y-[1.4875rem] xs:px-[1.1375rem] xs:py-[0.875rem] sm:space-y-[1.9125rem] sm:px-[1.4625rem] sm:py-[1.125rem]"
+      >
+        <li class="flex h-6 flex-row items-center justify-between xs:h-[2.1rem] sm:h-[2.7rem]">
+          <h4 class="text-base-group font-semibold leading-[1.5]">프로필 사진</h4>
+          <img
+            src="/assets/avatar-placeholder.png"
+            alt="프로필 이미지"
+            class="avatar-image aspect-square w-6 rounded-full [box-shadow:0.125rem_0.125rem_0.125rem_0px_rgba(0,_0,_0,_0.10)] xs:w-[2.1rem] sm:w-[2.7rem]"
+          />
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">이름(별명)</h4>
+          <span class="username text-sm-group font-semibold leading-[1.5]"></span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">하는일</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">프로필 키워드</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">성별</h4>
+          <span id="user-gender" class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">연령</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">회사</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">학교</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">자격</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+      </ul>
+    </section>
+    <!-- information -->
+
+    <!-- additional information -->
+    <section
+      class="rounded-t-[0.5rem] border-[0.01875rem] border-gray-300 shadow-md xs:rounded-t-[0.7rem] sm:rounded-t-[0.9rem]"
+    >
+      <div
+        class="flex flex-row items-center justify-between border-b-[0.01875rem] border-contentSecondary p-3 pr-4 xs:p-[1.05rem] xs:pr-[1.4rem] sm:p-[1.35rem] sm:pr-[1.8rem]"
+      >
+        <h3 class="text-base-group font-semibold leading-[1.5]">추가 정보</h3>
+        <a href="/pages/profile-detail" class="text-sm-group text-secondary">수정하기</a>
+      </div>
+      <ul
+        aria-label="추가 정보 리스트"
+        class="space-y-[1.0625rem] px-[0.8125rem] pb-8 pt-[0.625rem] xs:space-y-[1.4875rem] xs:px-[1.1375rem] xs:pb-[2.8rem] xs:pt-[0.875rem] sm:space-y-[1.9125rem] sm:px-[1.4625rem] sm:pb-[3.6rem] sm:pt-[1.125rem]"
+      >
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">소개</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">연관 링크</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">활동 분야</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+        <li class="flex flex-row items-center justify-between">
+          <h4 class="text-base-group font-semibold leading-[1.5]">업체 정보</h4>
+          <span class="text-sm-group font-semibold leading-[1.5] text-contentSecondary">미입력</span>
+        </li>
+      </ul>
+    </section>
+    <!-- additional information -->
+  </main>
+`;
+
+class EditProfile extends HTMLElement {
+  static observedAttributes = ['loading'];
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.loading = true;
+    this.currentUser = null;
+  }
+
+  connectedCallback() {
+    this.render();
+
+    // 첫 렌더링 후 데이터 fetching 시작하기
+    this.fetchData();
+  }
+
+  attributeChangedCallback(_attrName, _oldVal, _newVal) {
+    this.render();
+  }
+
+  get loading() {
+    return JSON.parse(this.getAttribute('loading'));
+  }
+
+  set loading(value) {
+    this.setAttribute('loading', JSON.stringify(value));
+  }
+
+  async fetchData() {
+    try {
+      await this.fetchCurrentUser();
+    } catch (error) {
+      // TODO: 유저에게 에러 메시지 보여주기
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async fetchCurrentUser() {
+    this.currentUser = await UserService.currentUser();
+  }
+
+  render() {
+    // 렌더링 전 초기화
+    this.shadowRoot.innerHTML = '';
+
+    if (this.loading) {
+      // this.shadowRoot.innerHTML = '<p>loading...</p>';
+      this.shadowRoot.innerHTML = '';
+      return;
+    }
+
+    // currentUser가 없으면 초기 시작 화면으로 redirect해둠, 따라서 로딩 이후 currentUser를 가져오지 못한 경우는 신경쓰지 않음.
+
+    this.shadowRoot.appendChild(EditProfileTemplate.content.cloneNode(true));
+
+    // template에 데이터 넣기
+    const $usernameSpans = this.shadowRoot.querySelectorAll('.username');
+    $usernameSpans.forEach(($usernameSpan) => {
+      $usernameSpan.textContent = this.currentUser.username;
+    });
+
+    const $userGender = this.shadowRoot.getElementById('user-gender');
+    if (this.currentUser.gender === 'M') {
+      $userGender.textContent = '남성';
+    } else if (this.currentUser.gender === 'F') {
+      $userGender.textContent = '여성';
+    } else {
+      $userGender.textContent = '미입력';
+    }
+
+    if (this.currentUser.avatar) {
+      const avatarImageUrl = getPbImageURL(this.currentUser, 'avatar');
+      const $avatarImages = this.shadowRoot.querySelectorAll('.avatar-image');
+      $avatarImages.forEach(($avatarImage) => {
+        $avatarImage.src = avatarImageUrl;
+      });
+    }
+  }
+}
+
+customElements.define('c-edit-profile', EditProfile);
--- src/pages/profile-detail/profile-detail.js
+++ src/pages/profile-detail/profile-detail.js
@@ -1,20 +1,6 @@
-const address = document.getElementById('address');
-const searchBtn = document.getElementById('search-btn');
-const agreeMatters = document.getElementById('agree-matters');
-const agreeCheckboxes = agreeMatters.querySelectorAll('input[type="checkbox"]');
-const saveBtn = document.getElementById('save-btn');
-
-console.log(agreeCheckboxes);
-
-function handleSearch() {
-  new daum.Postcode({
-    oncomplete: function (data) {
-      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-      // 예제를 참고하여 다양한 활용법을 확인해 보세요.
-      address.value = data.sigungu;
-    },
-  }).open();
-}
+import { UserService } from '@/service/UserService';
+import tailwindCSS from '/styles/tailwind.css?inline'; // css 파일 inline 가져오기
+import { getPbImageURL } from '@/api/getPbImageURL';
 
 function activeButton(btnNode, activeClass, disabledClass) {
   btnNode.removeAttribute('disabled');
@@ -27,30 +13,400 @@ function disableButton(btnNode, activeClass, disabledClass) {
   btnNode.classList.add(disabledClass, 'cursor-not-allowed');
 }
 
-function handleAgreeCheck(e) {
-  const target = e.target.closest('input[type="checkbox"]');
-  if (!target) return;
+const ProfileDetailTemplate = document.createElement('template');
+ProfileDetailTemplate.innerHTML = `
+  <style>${tailwindCSS}</style>
+  <form id="profile-form" name="profile-form" class="flex flex-col">
+    <section
+      class="flex flex-col gap-[1.875rem] px-3 py-8 xs:gap-[2.625rem] xs:px-[1.05rem] xs:py-[2.8rem] sm:gap-[3.375rem] sm:px-[1.35rem] sm:py-[3.6rem]"
+    >
+      <h2 class="sr-only">개인 정보</h2>
 
-  if (target.id === 'agree-all') {
-    if (target.checked) {
-      agreeCheckboxes.forEach((box) => {
-        box.checked = true;
-      });
+      <!-- 프로필 사진 -->
+      <div role="group" class="flex flex-col items-center">
+        <h2 class="sr-only">프로필 사진</h2>
+        <div class="relative w-1/4">
+          <img src="/assets/avatar-placeholder.png" alt="프로필 사진" id="avatar-image" class="aspect-square w-full rounded-full object-cover" />
+
+          <label
+            for="avatar"
+            class="absolute bottom-[0.06rem] right-[0.06rem] flex w-1/4 rounded-full bg-background p-[0.125rem] [box-shadow:0.25rem_0.25rem_0.25rem_0px_rgba(0,_0,_0,_0.15)] xs:bottom-[0.084rem] xs:right-[0.084rem] xs:p-[0.175rem] sm:bottom-[0.108rem] sm:right-[0.108rem] sm:p-[0.225rem]"
+          >
+            <img src="/icon/pencil.svg" alt="연필" aria-hidden="true" class="aspect-square w-full" />
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              accept=".jpg, .png, .svg, .gif, .webp"
+              class="sr-only"
+              aria-label="프로필사진 업로드"
+            />
+          </label>
+        </div>
+      </div>
+
+      <!-- 이름(별명) -->
+      <div role="group" class="flex w-full flex-col gap-2 xs:gap-[0.7rem] sm:gap-[0.9rem]">
+        <label for="username" class="text-base-group font-semibold leading-[150%]">이름(별명)</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="이름(별명)을 입력해주세요."
+          class="text-base-group rounded-lg border-[0.03125rem] border-solid border-contentPrimary px-4 py-2 leading-[160%] xs:rounded-[0.7rem] xs:px-[1.4rem] xs:py-[0.7rem] sm:rounded-[0.9rem] sm:px-[1.8rem] sm:py-[0.9rem]"
+        />
+      </div>
+
+      <!-- 성별 -->
+      <fieldset class="flex w-full flex-col">
+        <legend class="text-base-group mb-2 font-semibold leading-[150%] xs:mb-[0.7rem] sm:mb-[0.9rem]">
+          성별
+        </legend>
+
+        <div
+          role="radiogroup"
+          aria-label="성별 선택"
+          class="flex cursor-pointer rounded-lg bg-[#C4C7D2] xs:rounded-[0.7rem] sm:rounded-[0.9rem]"
+        >
+          <div class="flex-grow">
+            <input tabindex="0" type="radio" name="gender" id="male" value="M" class="peer sr-only" />
+            <label
+              for="male"
+              class="text-base-group flex cursor-pointer items-center justify-center rounded-lg py-[0.375rem] font-semibold text-background peer-checked:z-10 peer-checked:border peer-checked:border-tertiary peer-checked:bg-background peer-checked:text-secondary xs:rounded-[0.7rem] xs:py-[0.525rem] sm:rounded-[0.9rem] sm:py-[0.675rem]"
+            >
+              남자
+            </label>
+          </div>
+          <!-- js에서 버튼을 클릭하면 z-10, border-tertiary, border, bg-background, text-secondary를 클릭한 쪽으로 옮겨야함 -->
+          <div class="flex-grow">
+            <input tabindex="0" type="radio" name="gender" id="female" value="F" class="peer sr-only" />
+            <label
+              for="female"
+              aria-pressed="true"
+              class="text-base-group -ml-[0.3125rem] flex cursor-pointer items-center justify-center rounded-lg py-[0.375rem] font-semibold text-background peer-checked:z-10 peer-checked:border peer-checked:border-tertiary peer-checked:bg-background peer-checked:text-secondary xs:-ml-[0.4375rem] xs:rounded-[0.7rem] xs:py-[0.525rem] sm:-ml-[0.5625rem] sm:rounded-[0.9rem] sm:py-[0.675rem]"
+            >
+              여자
+            </label>
+          </div>
+        </div>
+      </fieldset>
+
+      <!-- 주소 -->
+      <div role="group" class="flex flex-col gap-2 xs:gap-[0.7rem] sm:gap-[0.9rem]">
+        <label for="address" class="text-base-group font-semibold leading-[150%]">주소</label>
+        <input
+          id="address"
+          type="text"
+          placeholder="검색하기 버튼을 통해 주소를 검색해주세요."
+          name="address"
+          readonly
+          class="text-base-group rounded-lg border-[0.03125rem] border-solid border-contentPrimary px-3 py-[0.5625rem] leading-[160%] xs:rounded-[0.7rem] xs:py-[0.7875rem] sm:rounded-[0.9rem] sm:py-[1.0125rem]"
+        />
+        <button
+          id="search-btn"
+          type="button"
+          class="flex items-center justify-center gap-1 rounded-lg border border-solid border-[#D3D3D3] px-[2.0625rem] py-2 xs:gap-[0.35rem] xs:rounded-[0.7rem] sm:gap-[0.45rem] sm:rounded-[0.9rem]"
+        >
+          <img
+            src="/icon/search.svg"
+            alt="돋보기 아이콘"
+            aria-hidden="true"
+            class="w-6 xs:w-[2.1rem] sm:w-[2.7rem]"
+          />
+          <span class="text-base-group font-semibold leading-[150%]">검색하기</span>
+        </button>
+      </div>
+    </section>
+
+    <!-- 동의 사항 -->
+    <fieldset id="agree-matters">
+      <legend class="sr-only">동의 사항</legend>
+
+      <!-- 전체 동의 -->
+      <div
+        role="group"
+        class="flex items-center justify-between border-t border-solid border-contentSecondary px-3 py-[0.875rem] xs:px-[1.05rem] xs:py-[1.225rem] sm:px-[1.35rem] sm:py-[1.575rem]"
+      >
+        <div class="flex items-center">
+          <input
+            type="checkbox"
+            id="agree-all"
+            name="agree-all"
+            class="mr-3 min-h-4 min-w-4 cursor-pointer xs:mr-[1.05rem] xs:min-h-[1.4rem] xs:min-w-[1.4rem] sm:mr-[1.35rem] sm:min-h-[1.8rem] sm:min-w-[1.8rem]"
+          />
+          <label for="agree-all" class="text-base-group font-semibold leading-[150%]"
+            >아래 내용에 전체 동의합니다.</label
+          >
+        </div>
+        <span class="text-sm-group leading-[160%] text-negative">필수동의</span>
+      </div>
+
+      <div
+        class="flex flex-col gap-[1.125rem] border-t-[0.03rem] border-solid border-contentSecondary px-3 pb-2 pt-4 xs:gap-[1.575rem] xs:px-[1.05rem] xs:pb-[0.7rem] xs:pt-[1.4rem] sm:gap-[2.025rem] sm:px-[1.35rem] sm:pb-[0.9rem] sm:pt-[1.8rem]"
+      >
+        <!-- 항목 1 -->
+        <div role="group">
+          <div class="mb-[0.5625rem] flex items-center xs:mb-[0.7875rem] sm:mb-[1.0125rem]">
+            <input
+              type="checkbox"
+              id="agree-collection"
+              name="agree-collection"
+              required
+              class="mr-3 min-h-4 min-w-4 cursor-pointer xs:mr-[1.05rem] xs:min-h-[1.4rem] xs:min-w-[1.4rem] sm:mr-[1.35rem] sm:min-h-[1.8rem] sm:min-w-[1.8rem]"
+            />
+            <label for="agree-collection" class="text-sm-group font-semibold leading-[150%]">
+              [ 필수 ] 개인 정보 수집 및 이용 동의
+            </label>
+          </div>
+          <p class="text-sm-group ml-7 leading-[160%] text-contentSecondary xs:ml-[2.45rem] sm:ml-[3.15rem]">
+            입력한 별명, 프로필 사진, 하는 일, 프로필 키워드, 성별, 연령, 회사, 학교, 자격은 엔터이듬 서비스 내
+            프로필 페이지, 답변자 정보 영역, 엔터이듬 홈 및 서비스 내 프로필 공개를 목적으로 합니다.
+          </p>
+          <p class="text-sm-group ml-7 font-semibold leading-[160%] text-secondary xs:ml-[2.45rem] sm:ml-[3.15rem]">
+            수집된 정보는 언제든지 직접 삭제할 수 있고, 탈퇴 시에는 바로 파기됩니다.
+          </p>
+        </div>
+
+        <!-- 항목 2 -->
+        <div role="group" class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              type="checkbox"
+              id="agree-promotion"
+              name="agree-promotion"
+              class="mr-3 min-h-4 min-w-4 cursor-pointer xs:mr-[1.05rem] xs:min-h-[1.4rem] xs:min-w-[1.4rem] sm:mr-[1.35rem] sm:min-h-[1.8rem] sm:min-w-[1.8rem]"
+            />
+            <label for="agree-promotion" class="text-sm-group font-semibold leading-[150%]">
+              [ 필수 ] 프로필 정보 노출 영역 확인
+            </label>
+          </div>
+          <a
+            href="#"
+            aria-label="프로필 정보 노출 영역 확인 자세히 보기"
+            class="text-sm-group font-semibold leading-[150%] text-contentSecondary"
+          >
+            자세히&gt;
+          </a>
+        </div>
+
+        <div role="group" class="flex">
+          <input
+            type="checkbox"
+            id="agree-marketing"
+            name="agree-marketing"
+            class="mr-3 min-h-4 min-w-4 cursor-pointer xs:mr-[1.05rem] xs:min-h-[1.4rem] xs:min-w-[1.4rem] sm:mr-[1.35rem] sm:min-h-[1.8rem] sm:min-w-[1.8rem]"
+          />
+          <label for="agree-marketing" class="text-sm-group font-semibold leading-[150%]">
+            [ 필수 ] 사실과 다른 정보를 기재하여 발생한 문제에 대해서는 본인이 일체의 책임을 부담하겠습니다.
+          </label>
+        </div>
+      </div>
+    </fieldset>
+
+    <div class="flex flex-col items-center gap-[1.125rem] p-3 xs:p-[1.05rem] sm:p-[1.35rem]">
+      <div class="flex w-full gap-[0.5625rem] xs:gap-[0.7875rem] sm:gap-[1.0125rem]">
+        <a
+          role="button"
+          href="/pages/edit-profile/"
+          class="text-base-group text-center flex-grow rounded-lg border border-solid border-[#D3D3D3] bg-[#E9E9E9] py-2 font-semibold leading-[150%] xs:rounded-[0.7rem] xs:py-[0.7rem] sm:rounded-[0.9rem] sm:py-[0.9rem]"
+        >
+          취소
+        </a>
+
+        <button
+          id="save-btn"
+          type="submit"
+          disabled
+          class="text-base-group flex-grow cursor-not-allowed rounded-lg border border-solid border-[#D3D3D3] bg-[#D8D9E1] py-2 font-semibold leading-[150%] xs:rounded-[0.7rem] xs:py-[0.7rem] sm:rounded-[0.9rem] sm:py-[0.9rem]"
+        >
+          저장
+        </button>
+      </div>
+      <a href="#" class="text-sm-group leading-[160%] text-contentSecondary">정보 초기화 및 이용 동의 철회</a>
+    </div>
+  </form>
+
+  <!-- 모달창 배경 -->
+  <div hidden class="absolute left-0 top-0 z-50 h-screen w-full bg-contentTertiary bg-opacity-20">
+    <!-- 모달창 -->
+    <dialog
+      aria-modal="true"
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+      class="absolute left-1/2 top-1/2 flex w-[70%] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-5 rounded-[1.25rem] bg-background px-[1.125rem] pb-[1.125rem] pt-5 [box-shadow:0_0.25rem_0.25rem_0_rgba(0,_0,_0,_0.25)]"
+    >
+      <h2 id="modal-title" class="text-base-group leading-[160%]">저장 완료!</h2>
+      <p id="modal-description" class="text-sm-group leading-[160%] text-contentSecondary">
+        내 질문&답변의 프로필 카드, 프로필 홈에서 변경된 프로필 정보를 확인 할 수 있어요.
+      </p>
+      <button
+        aria-label="확인 및 모달 닫기"
+        class="text-base-group w-full cursor-pointer rounded-lg bg-primary py-2 text-background"
+      >
+        확인
+      </button>
+    </dialog>
+  </div>
+`;
+
+class ProfileDetail extends HTMLElement {
+  static observedAttributes = ['loading'];
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.loading = true;
+    this.currentUser = null;
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addEventListener('click', this.handleClick);
+    // 첫 렌더링 후 데이터 fetching 시작하기
+    this.fetchData();
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this.handleClick);
+  }
+
+  handleSearch() {
+    const address = this.shadowRoot.getElementById('address');
+
+    new daum.Postcode({
+      oncomplete: function (data) {
+        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+        // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+        address.value = data.sigungu;
+      },
+    }).open();
+  }
+
+  handleAgreeCheck(e, $agreeMatters) {
+    const agreeCheckboxes = $agreeMatters.querySelectorAll('input[type="checkbox"]');
+    const saveBtn = this.shadowRoot.getElementById('save-btn');
+
+    const target = e.composedPath()[0].closest('input[type="checkbox"]');
+    if (!target) return;
+
+    if (target.id === 'agree-all') {
+      if (target.checked) {
+        agreeCheckboxes.forEach((box) => {
+          box.checked = true;
+        });
+      } else {
+        agreeCheckboxes.forEach((box) => {
+          box.checked = false;
+        });
+      }
+    }
+
+    if (agreeCheckboxes[1].checked && agreeCheckboxes[2].checked && agreeCheckboxes[3].checked) {
+      agreeCheckboxes[0].checked = true;
+      activeButton(saveBtn, 'bg-secondary', 'bg-[#D8D9E1]');
     } else {
-      agreeCheckboxes.forEach((box) => {
-        box.checked = false;
-      });
+      agreeCheckboxes[0].checked = false;
+      disableButton(saveBtn, 'bg-secondary', 'bg-[#D8D9E1]');
     }
   }
 
-  if (agreeCheckboxes[1].checked && agreeCheckboxes[2].checked && agreeCheckboxes[3].checked) {
-    agreeCheckboxes[0].checked = true;
-    activeButton(saveBtn, 'bg-secondary', 'bg-[#D8D9E1]');
-  } else {
-    agreeCheckboxes[0].checked = false;
-    disableButton(saveBtn, 'bg-secondary', 'bg-[#D8D9E1]');
+  handleClick = (e) => {
+    const $searchBtn = this.shadowRoot.getElementById('search-btn');
+    if (e.composedPath().includes($searchBtn)) {
+      this.handleSearch();
+    }
+
+    const $agreeMatters = this.shadowRoot.getElementById('agree-matters');
+    if (e.composedPath().includes($agreeMatters)) {
+      this.handleAgreeCheck(e, $agreeMatters);
+    }
+  };
+
+  handleAvatarInputChange = (e) => {
+    const $avatarImage = this.shadowRoot.getElementById('avatar-image');
+    $avatarImage.src = URL.createObjectURL(e.target.files[0]);
+    $avatarImage.onload = function () {
+      URL.revokeObjectURL($avatarImage.src);
+    };
+  };
+
+  handleProfileFormSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const $avatarInput = this.shadowRoot.getElementById('avatar');
+
+    await UserService.updateUser(this.currentUser.id, formData);
+
+    window.location.href = '/pages/edit-profile/';
+  };
+
+  attributeChangedCallback(_attrName, _oldVal, _newVal) {
+    this.render();
+  }
+  get loading() {
+    return JSON.parse(this.getAttribute('loading'));
+  }
+
+  set loading(value) {
+    this.setAttribute('loading', JSON.stringify(value));
+  }
+
+  async fetchData() {
+    try {
+      await this.fetchCurrentUser();
+    } catch (error) {
+      // TODO: 유저에게 에러 메시지 보여주기
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async fetchCurrentUser() {
+    this.currentUser = await UserService.currentUser();
+  }
+
+  render() {
+    // 렌더링 전 초기화
+    this.shadowRoot.innerHTML = '';
+
+    if (this.loading) {
+      // this.shadowRoot.innerHTML = '<p>loading...</p>';
+      this.shadowRoot.innerHTML = '';
+      return;
+    }
+
+    // currentUser가 없으면 초기 시작 화면으로 redirect해둠, 따라서 로딩 이후 currentUser를 가져오지 못한 경우는 신경쓰지 않음.
+
+    this.shadowRoot.appendChild(ProfileDetailTemplate.content.cloneNode(true));
+
+    // template에 데이터 넣기
+    const $usernameInput = this.shadowRoot.getElementById('username');
+    $usernameInput.value = this.currentUser.username;
+
+    if (this.currentUser.gender === 'M') {
+      this.shadowRoot.getElementById('male').checked = true;
+    } else if (this.currentUser.gender === 'F') {
+      this.shadowRoot.getElementById('female').checked = true;
+    }
+
+    const $userAddressInput = this.shadowRoot.getElementById('address');
+    $userAddressInput.value = this.currentUser.address;
+
+    if (this.currentUser.avatar) {
+      const $avatarImage = this.shadowRoot.getElementById('avatar-image');
+      $avatarImage.src = getPbImageURL(this.currentUser, 'avatar');
+    }
+
+    const $avatarInput = this.shadowRoot.getElementById('avatar');
+    $avatarInput.addEventListener('change', this.handleAvatarInputChange);
+
+    const $profileForm = this.shadowRoot.getElementById('profile-form');
+    $profileForm.addEventListener('submit', this.handleProfileFormSubmit);
   }
 }
 
-searchBtn.addEventListener('click', handleSearch);
-agreeMatters.addEventListener('click', handleAgreeCheck);
+customElements.define('c-profile-detail', ProfileDetail);
