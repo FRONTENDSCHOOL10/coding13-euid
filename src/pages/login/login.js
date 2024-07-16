3b9eb57ca32e8166ef3a70a84e89bc95cac26404
jisoolee <szjslee@gmail.com>
2024-07-16 17:29:19 +0900

    modify: login.js, signup.js 수정

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -27,30 +27,29 @@ function login() {
     verifyButton.disabled = !isValid;
 
     if (isValid) {
-      verifyButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        const phoneNumber = phoneInput.value.replace(/\s/g, '');
-
-        if (!verifyButton.disabled) {
-          // 6자리 랜덤 코드
-          const verificationCode = Math.floor(100000 + Math.random() * 900000);
-
-          localStorage.setItem('phoneNumber', phoneNumber);
-          localStorage.setItem('verificationCode', verificationCode);
-
-          // 임시
-          alert(`인증 코드: ${verificationCode}`);
-
-          // login2.html로 이동 (인증번호 입력하는 페이지로 이동)
-          window.location.href = '/pages/login/login2.html';
-        }
-      });
-
       verifyButton.classList.remove('border-contentSecondary', 'text-contentSecondary');
       verifyButton.classList.add('border-black', 'text-black');
     } else {
       verifyButton.classList.remove('border-black', 'text-black');
       verifyButton.classList.add('border-contentSecondary', 'text-contentSecondary');
+    }
+  });
+  verifyButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const phoneNumber = phoneInput.value.replace(/\s/g, '');
+
+    if (!verifyButton.disabled) {
+      // 6자리 랜덤 코드
+      const verificationCode = Math.floor(100000 + Math.random() * 900000);
+
+      localStorage.setItem('phoneNumber', phoneNumber);
+      localStorage.setItem('verificationCode', verificationCode);
+
+      // 임시
+      alert(`인증 코드: ${verificationCode}`);
+
+      // login2.html로 이동 (인증번호 입력하는 페이지로 이동)
+      window.location.href = '/pages/login/login2.html';
     }
   });
 }
