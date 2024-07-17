5d4c504611d5cfe9ceab134a5b3b348589602ba8
rewind122 <luvyoudear12@gmail.com>
2024-07-17 20:38:29 +0900

    modify: 검색 결과 페이지 렌더링 코드 수정

--- src/pages/search-result/search-result.js
+++ src/pages/search-result/search-result.js
@@ -113,13 +113,14 @@ async function searchResultPage() {
 
     const postList = await pb.collection('posts').getFullList({
       sort,
+      expand: 'user_id',
       filter: `(${searchText})${option}`,
     });
 
     exchangeList.textContent = '';
 
     for (let item of postList) {
-      const postCreator = await pb.collection('users').getOne(item.user_id);
+      const postCreator = item.expand.user_id;
 
       // 좋아요 수에 각 게시글에 저장된 좋아요 개수를 불러오도록 수정해야 합니다. (pocketbase 항목 추가 필요)
       const template = `
