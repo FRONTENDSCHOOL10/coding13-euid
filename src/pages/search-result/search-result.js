47a6bfdf6a0ab2fb0eea6f45f8102687d05efdd2
rewind122 <luvyoudear12@gmail.com>
2024-07-18 00:01:56 +0900

    refactor: 검색 결과 페이지 리팩토링
    - 코드 수정 및 주석 추가

--- src/pages/search-result/search-result.js
+++ src/pages/search-result/search-result.js
@@ -88,15 +88,13 @@ async function searchResultPage() {
       const nowTimestamp = now.getTime();
 
       const seconds = Math.floor((nowTimestamp - createTimestamp) / 1000);
-      if (seconds < 60) return '방금 전';
-
       const minutes = seconds / 60;
-      if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-
       const hours = minutes / 60;
-      if (hours < 24) return `${Math.floor(hours)}시간 전`;
-
       const days = hours / 24;
+
+      if (seconds < 60) return '방금 전';
+      if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+      if (hours < 24) return `${Math.floor(hours)}시간 전`;
       if (days < 7) return `${Math.floor(days)}일 전`;
       else if (days < 30) return `${Math.floor(days / 7)}주 전`;
       else if (days < 365) return `${Math.floor(days / 30)}달 전`;
@@ -124,43 +122,44 @@ async function searchResultPage() {
 
       // 좋아요 수에 각 게시글에 저장된 좋아요 개수를 불러오도록 수정해야 합니다. (pocketbase 항목 추가 필요)
       const template = `
-      <li>
-        <a
-          href="/pages/exchange-detail/index.html?post=${item.id}"
-          class="relative flex gap-3 border-b border-gray-200 bg-background p-3 outline-0 focus:shadow-[inset_0_0_0_2px_#719cf7] xs:gap-[1.05rem] xs:p-[1.05rem] sm:gap-[1.35rem] sm:p-[1.35rem]"
-        >
-          <figure
-            class="aspect-square w-[5.625rem] flex-shrink-0 overflow-hidden rounded-md bg-gray-200 xs:w-[7.875rem] sm:w-[10.125rem]"
+        <li>
+          <a
+            href="/pages/exchange-detail/index.html?post=${item.id}"
+            class="relative flex gap-3 border-b border-gray-200 bg-background p-3 outline-0 focus:shadow-[inset_0_0_0_2px_#719cf7] xs:gap-[1.05rem] xs:p-[1.05rem] sm:gap-[1.35rem] sm:p-[1.35rem]"
           >
-            <img class="object-cover" src="${getPbImagesURL(item, 0)}" alt="거래 물품 대표 사진" />
-          </figure>
-          <section class="flex w-full flex-col justify-center overflow-hidden" aria-label="거래 물품 정보">
-            <h2 class="text-base-group truncate leading-[1.6] text-contentPrimary">
-              ${item.title}
-            </h2>
-            <span class="text-sm-group leading-[1.6] text-contentTertiary">${postCreator.address} · </span>
-            <div class="flex items-center gap-2 xs:gap-[0.7rem] sm:gap-[0.9rem]" role="group">
-              <!-- 거래 상태 -->
-              <span
-                class="text-sm-group rounded px-1 py-0.5 font-semibold leading-normal text-background xs:px-[0.35rem] xs:py-[0.175rem] sm:px-[0.45rem] sm:py-[0.225rem]"
-              >
-                <span class="sr-only">거래상태</span>
-              </span>
-              <!-- // 거래 상태 -->
-              <span class="text-base-group font-semibold leading-normal">
-                <span class="sr-only">물품가격</span>${item.price.toLocaleString()}원
-              </span>
-            </div>
-          </section>
-          <span
-            class="text-sm-group absolute bottom-2 right-3 flex items-center gap-0.5 leading-[1.6] xs:bottom-[0.7rem] xs:right-[1.05rem] xs:gap-[0.175rem] sm:bottom-[0.9rem] sm:right-[1.35rem] sm:gap-[0.225rem]"
-          >
-            <img src="/icon/heart.svg" alt="좋아요" class="w-[0.875rem] xs:w-[1.225rem] sm:w-[1.575rem]" />
-            4
-          </span>
-        </a>
-      </li>
-    `;
+            <figure
+              class="aspect-square w-[5.625rem] flex-shrink-0 overflow-hidden rounded-md bg-gray-200 xs:w-[7.875rem] sm:w-[10.125rem]"
+            >
+              <img class="object-cover" src="${getPbImagesURL(item, 0)}" alt="${item.title} 대표 사진" />
+            </figure>
+            <section class="flex w-full flex-col justify-center overflow-hidden">
+              <h2 class="text-base-group truncate leading-[1.6] text-contentPrimary">
+                ${item.title}
+              </h2>
+              <span class="text-sm-group leading-[1.6] text-contentTertiary">${postCreator.address} · </span>
+              <div class="flex items-center gap-2 xs:gap-[0.7rem] sm:gap-[0.9rem]" role="group">
+                <!-- 거래 상태 -->
+                <span
+                  class="text-sm-group rounded px-1 py-0.5 font-semibold leading-normal text-background xs:px-[0.35rem] xs:py-[0.175rem] sm:px-[0.45rem] sm:py-[0.225rem]"
+                >
+                  <span class="sr-only">거래상태</span>
+                </span>
+                <!-- // 거래 상태 -->
+                <span class="text-base-group font-semibold leading-normal">
+                  <span class="sr-only">물품가격</span>${item.price.toLocaleString()}원
+                </span>
+              </div>
+            </section>
+            <span
+              class="text-sm-group absolute bottom-2 right-3 flex items-center gap-0.5 leading-[1.6] xs:bottom-[0.7rem] xs:right-[1.05rem] xs:gap-[0.175rem] sm:bottom-[0.9rem] sm:right-[1.35rem] sm:gap-[0.225rem]"
+              aria-label="좋아요 개수"
+            >
+              <img src="/icon/heart.svg" alt="" class="w-[0.875rem] xs:w-[1.225rem] sm:w-[1.575rem]" />
+              4
+            </span>
+          </a>
+        </li>
+      `;
 
       exchangeList.insertAdjacentHTML('afterbegin', template);
 
