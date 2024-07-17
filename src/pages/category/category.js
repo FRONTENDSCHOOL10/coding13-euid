c02b22edb320e8c97da65aa4ee4c18a613bdd3c3
jisoolee <szjslee@gmail.com>
2024-07-17 16:26:25 +0900

    feat: 카테고리 페이지 기능 구현 - localStorage에 저장 - 최대 다섯 개 선택으로 제한

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,18 +1,51 @@
 const categoryButton = document.querySelectorAll('.category-button');
+let selectedCategories = [];
 
 categoryButton.forEach((button) => {
   const buttonText = button.querySelector('.category').textContent;
-  const storageButton = `category-${buttonText}`;
+  const storageKey = `${buttonText}`;
+
+  const saveState = localStorage.getItem(storageKey);
+  if (saveState === 'active') {
+    activateButton(button);
+    selectedCategories.push(buttonText);
+  }
 
   button.addEventListener('click', () => {
-    const img = button.querySelector('img');
-    const span = button.querySelector('span');
+    const isActive = button.classList.contains('bg-primary');
 
-    button.classList.toggle('bg-[#A7A7A7]');
-    button.classList.toggle('bg-primary');
-    span.classList.toggle('text-white');
-    span.classList.toggle('text-black');
-
-    img.src = button.classList.contains('bg-primary') ? '/icon/check.svg' : '/icon/category-plus.svg';
+    if (isActive) {
+      deactivateButton(button);
+      localStorage.setItem(storageKey, 'inactive');
+      selectedCategories = selectedCategories.filter((category) => category !== buttonText);
+    } else {
+      if (selectedCategories.length < 5) {
+        activateButton(button);
+        localStorage.setItem(storageKey, 'active');
+        selectedCategories.push(buttonText);
+      } else {
+        alert('최대 다섯 개만 선택하실 수 있습니다.');
+      }
+    }
   });
 });
+
+function activateButton(button) {
+  const img = button.querySelector('img');
+  const span = button.querySelector('.category');
+
+  button.classList.remove('bg-[#A7A7A7]');
+  button.classList.add('bg-primary');
+  span.classList.add('text-white');
+  img.src = '/icon/check.svg';
+}
+
+function deactivateButton(button) {
+  const img = button.querySelector('img');
+  const span = button.querySelector('.category');
+
+  button.classList.remove('bg-primary');
+  button.classList.add('bg-[#A7A7A7]');
+  span.classList.remove('text-white');
+  img.src = '/icon/category-plus.svg';
+}
