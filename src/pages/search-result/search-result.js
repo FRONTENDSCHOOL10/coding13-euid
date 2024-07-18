93e65ab60430f4f9d1ea726c2f83b7157377b35c
JoowonJang <wndnjs5248@hanmail.net>
2024-07-19 03:36:12 +0900

    feature: search-result 페이지 필터 및 정렬 중첩기능 구현
    - option들을 하나의 문자열에 담아서 사용

--- src/pages/search-result/search-result.js
+++ src/pages/search-result/search-result.js
@@ -40,6 +40,9 @@ async function searchResultPage() {
   const searchParams = new URLSearchParams(location.search);
   const recentSearchText = searchParams.get('search');
 
+  /* ------------------------------ 중첩 필터링을 위한 문자열 ----------------------------- */
+  let optionString = '';
+
   // 검색어를 검색창에 입력
   searchInput.value = recentSearchText;
 
@@ -111,9 +114,9 @@ async function searchResultPage() {
 
   // 렌더링 함수
   async function renderSearchResult(option, sort = 'created') {
+    console.log(optionString);
     const searchTextArray = recentSearchText.split(' ');
-    const searchText = searchTextArray.map((item) => `title ~ '${item}'`).join(' && ');
-
+    const searchText = searchTextArray.map((item) => `title ~ "${item}"`).join(' && ');
     const postList = await pb.collection('posts').getFullList({
       sort,
       expand: 'user_id',
@@ -191,9 +194,11 @@ async function searchResultPage() {
   // '거래가능만 보기' 체크 시
   possibleCheck.addEventListener('click', () => {
     if (possibleCheck.checked) {
-      renderSearchResult(" && state = ''").then(noSearchResult);
+      optionString += " && state = ''";
+      renderSearchResult(optionString).then(noSearchResult);
     } else {
-      renderSearchResult('').then(noSearchResult);
+      optionString = optionString.replace(" && state = ''", '');
+      renderSearchResult(optionString).then(noSearchResult);
     }
   });
 
@@ -256,7 +261,7 @@ async function searchResultPage() {
 
     // 어떤 카테고리도 선택되지 않았을 때
     if (checkedCategories.length === 0) {
-      renderSearchResult('').then(noSearchResult);
+      renderSearchResult(optionString).then(noSearchResult);
       return;
     }
 
@@ -265,9 +270,9 @@ async function searchResultPage() {
       filteringTextArray.push(item.nextElementSibling.textContent);
     }
 
-    const filteringText = filteringTextArray.map((item) => `category = '${item}'`).join(' || ');
-
-    renderSearchResult(` && (${filteringText})`).then(noSearchResult);
+    const filteringText = filteringTextArray.map((item) => `category = "${item}"`).join(' || ');
+    optionString += ` && ${filteringText}`;
+    renderSearchResult(optionString).then(noSearchResult);
   }
 
   // 카테고리 선택 초기화 함수
@@ -298,7 +303,8 @@ async function searchResultPage() {
     }
 
     modalPrice.close();
-    renderSearchResult(priceOption).then(noSearchResult);
+    optionString += priceOption;
+    renderSearchResult(optionString).then(noSearchResult);
   }
 
   // 가격 범위 입력 초기화 함수
@@ -334,9 +340,9 @@ async function searchResultPage() {
     modalOrder.close();
 
     if (selectText === '오래된순') {
-      renderSearchResult('', '-created').then(noSearchResult);
+      renderSearchResult(optionString, '-created').then(noSearchResult);
     } else {
-      renderSearchResult('').then(noSearchResult);
+      renderSearchResult(optionString).then(noSearchResult);
     }
   }
 
