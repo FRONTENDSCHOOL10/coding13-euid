cd7cabcd463fd99e10104e67ee8aefd4d1252ec4
JoowonJang <wndnjs5248@hanmail.net>
2024-07-19 02:43:50 +0900

    refactor: main 페이지 판매글로 변경
    - posts의 interested 오름차순으로 정렬 후 1~2번째는 슬라이드, 3~10번째는 글 목록으로 렌더링

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import '/components/spinner/spinner';
 import { UserService } from '/service/UserService.js';
 import Swiper from 'swiper';
 import { Autoplay, Pagination } from 'swiper/modules';
+import { getPbImagesURL } from '/api/getPbImageURL.js';
+import pb from '/api/pocketbase.js';
 
 const MainTemplate = document.createElement('template');
 MainTemplate.innerHTML = `
@@ -24,170 +26,16 @@ MainTemplate.innerHTML = `
 <!-- 추천 글 슬라이더 -->
 <div role="region" aria-label="추천 글 슬라이더" class="swiper h-[66.875vw]">
   <div class="swiper-wrapper">
-    <!-- 슬라이드 1 -->
-    <article aria-roledescription="slide" class="swiper-slide relative bg-contentTertiary">
-      <div class="p-5">
-        <h2 id="slide1-title" class="text-lg-group font-semibold text-background">
-          풀리지 않는 버그 속<br />개발자로 살아남기
-        </h2>
-        <a href="#" aria-describedby="slide1-title" class="text-sm-group text-background">자세히 보기</a>
-      </div>
-      <img
-        src="/slideImage.webp"
-        alt="슬라이드1"
-        class="absolute bottom-0 right-0 w-[63.125vw] [aspect-ratio:392/372]"
-      />
-    </article>
-    <!-- 슬라이드 2 -->
-    <article aria-roledescription="slide" class="swiper-slide relative bg-contentTertiary">
-      <div class="p-5">
-        <h2 id="slide1-title" class="text-lg-group font-semibold text-background">
-          풀리지 않는 버그 속<br />개발자로 살아남기
-        </h2>
-        <a href="#" aria-describedby="slide1-title" class="text-sm-group text-background">자세히 보기</a>
-      </div>
-      <img
-        src="/slideImage.webp"
-        alt="슬라이드2"
-        class="absolute bottom-0 right-0 w-[63.125vw] [aspect-ratio:392/372]"
-      />
-    </article>
   </div>
   <div role="group" aria-label="슬라이드 페이지네이션" class="swiper-pagination"></div>
 </div>
 
 <!-- 글 목록 -->
 <ul
+  id="post-list"
   aria-label="전체 글 목록"
   class="flex flex-wrap justify-between px-4 py-3 xs:px-[1.4rem] xs:py-[1.05rem] sm:px-[1.8rem] sm:py-[1.35rem]"
->
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글1"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글2"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글3"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글4"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글5"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-  <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
-    <article>
-      <img
-        src="/storyImage.webp"
-        alt="메인 글6"
-        class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem]"
-      />
-      <a href="#">
-        <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
-          디자인에 대해 하나도 알지 못했던 내가 취업할 수 있었던 이유
-        </h2>
-        <p class="text-sm-group leading-[160%] text-contentSecondary">
-          <span class="sr-only">카테고리: </span>
-          UIUX
-          <span aria-hidden="true">&#124;</span>
-          <span class="sr-only">작성자: </span>
-          1기 수강생 박치열
-        </p>
-      </a>
-    </article>
-  </li>
-</ul>
+></ul>
 
 <!-- 네비게이션 -->
 <c-navigation data-page="홈"></c-navigation>
@@ -236,7 +84,7 @@ class Main extends HTMLElement {
     try {
       this.loading = true;
       this.error = null;
-      await this.fetchCurrentUser();
+      await Promise.all([this.fetchCurrentUser(), this.fetchPosts()]);
     } catch (error) {
       this.error = error?.message
         ? `에러가 발생했습니다: ${error.message}. 잠시 후 새로고침 해주세요.`
@@ -250,7 +98,15 @@ class Main extends HTMLElement {
     this.currentUser = await UserService.currentUser();
   }
 
-  render() {
+  async fetchPosts() {
+    const list = await pb.collection('posts').getList(1, 10, {
+      sort: 'interested',
+      expand: 'user_id',
+    });
+    this.items = list.items;
+  }
+
+  async render() {
     // 렌더링 전 초기화
     this.shadowRoot.innerHTML = '';
 
@@ -263,6 +119,54 @@ class Main extends HTMLElement {
     }
 
     this.shadowRoot.appendChild(MainTemplate.content.cloneNode(true));
+
+    const postList = this.shadowRoot.getElementById('post-list');
+    this.items.forEach((item, index) => {
+      if (index > 1) {
+        const liTemplate = `
+          <li class="mb-7 w-[43.125vw] xs:mb-[2.45rem] sm:mb-[3.15rem]">
+            <article>
+              <a href="#">
+                <img
+                  src=${getPbImagesURL(item, 0)}
+                  alt="메인 글${index - 1}"
+                  class="mb-3 w-[43.125vw] rounded-[5.797101449275362%] [aspect-ratio:8.625/6.5] xs:mb-[1.05rem] sm:mb-[1.35rem] border object-cover"
+                />
+                <h2 class="text-sm-group mb-1 leading-[160%] xs:mb-[0.35rem] sm:mb-[0.45rem]">
+                  ${item.title}
+                </h2>
+                <p class="text-sm-group leading-[160%] text-contentSecondary">
+                  <span class="sr-only">카테고리: </span>
+                  ${item.expand.user_id.category[0]}
+                  <span aria-hidden="true">&#124;</span>
+                  <span class="sr-only">작성자: </span>
+                  ${item.expand.user_id.username}
+                </p>
+              </a>
+            </article>
+          </li>
+          `;
+
+        postList.insertAdjacentHTML('beforeend', liTemplate);
+      } else {
+        const swiperTemplate = `
+          <article aria-roledescription="slide" class="swiper-slide relative border">
+            <div class="p-5 xs:p-[1.75rem] sm:p-[2.25rem]">
+              <h2 id="slide1-title" class="text-lg-group font-semibold">
+                ${item.title}
+              </h2>
+              <a href="/pages/exchange-detail/index.html?post=${item.id}" aria-describedby="slide1-title" class="text-sm-group">자세히 보기</a>
+            </div>
+            <img
+              src=${getPbImagesURL(item, 0)}
+              alt="슬라이드${index + 1}"
+              class="absolute bottom-0 right-0 w-[50vw] aspect-square"
+            />
+          </article>
+        `;
+        this.shadowRoot.querySelector('.swiper-wrapper').insertAdjacentHTML('beforeend', swiperTemplate);
+      }
+    });
 
     const swiper = new Swiper(this.shadowRoot.querySelector('.swiper'), {
       modules: [Pagination, Autoplay],
@@ -277,17 +181,7 @@ class Main extends HTMLElement {
         renderFraction: function (currentClass, totalClass) {
           return `
             <span aria-label="현재 슬라이드 번호" class="${currentClass}"></span>
-            <svg aria-hidden="true" width="41" height="2" viewBox="0 0 41 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g clip-path="url(#clip0_238_3875)">
-              <rect y="0.5" width="41" height="1" fill="white"/>
-              <rect y="0.5" width="9" height="1" fill="white"/>
-              </g>
-              <defs>
-              <clipPath id="clip0_238_3875">
-              <rect width="41" height="1" fill="white" transform="translate(0 0.5)"/>
-              </clipPath>
-              </defs>
-            </svg>
+            <div class="w-10 xs:w-[3.5rem] sm:w-[4.5rem] h-[1px] bg-black mx-1"></div>
             <span aria-label="전체 슬라이드 수" class="${totalClass}"></span>
           `;
         },
